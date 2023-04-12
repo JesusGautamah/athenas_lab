de3be2e94414289234f50993f875b15b4f00e6b6
JesusGautamah <lima.jesuscc@gmail.com>
2023-04-12 11:24:36 -0300

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,7 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
   config.assets.css_compressor = nil
+  config.assets.compile = false
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
